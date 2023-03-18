abc9c31a394a9ecd175479d78887821c6ce77755
Pandula <pandula95@gmail.com>
2023-03-19 01:43:35 +0530

    style: :lipstick: Complete modal footer

--- src/components/TableModal/TableModal.js
+++ src/components/TableModal/TableModal.js
@@ -117,7 +117,7 @@ const TableModal = (props) => {
               </div>
 
               {/* Height and Scrollable Auto */}
-              <div style={{ height: "85%", overflowY: "scroll" }}>
+              <div style={{ maxHeight: "62vh", overflowY: "scroll" }}>
                 <div className="modal-drawer_right__element">
                   {" "}
                   <svg
@@ -263,13 +263,14 @@ const TableModal = (props) => {
               <path fill="none" d="M0 0h24v24H0z" />
               <path d="M10 8h4V6.5a3.5 3.5 0 1 1 3.5 3.5H16v4h1.5a3.5 3.5 0 1 1-3.5 3.5V16h-4v1.5A3.5 3.5 0 1 1 6.5 14H8v-4H6.5A3.5 3.5 0 1 1 10 6.5V8zM8 8V6.5A1.5 1.5 0 1 0 6.5 8H8zm0 8H6.5A1.5 1.5 0 1 0 8 17.5V16zm8-8h1.5A1.5 1.5 0 1 0 16 6.5V8zm0 8v1.5a1.5 1.5 0 1 0 1.5-1.5H16zm-6-6v4h4v-4h-4z" />
             </svg>
+            {/* prettier-ignore */}
             <p>Cmd</p>
           </div>
           <div>
             <p>+</p>
           </div>
           <div className="footer-cmd">
-            <p>1 + 9</p>
+            <p>1-9</p>
           </div>
           <div>
             <p>Quick select space/stack/link</p>

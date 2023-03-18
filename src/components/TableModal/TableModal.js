545f1e01a897787a9ab00a315a3739719f18ca52
Pandula <pandula95@gmail.com>
2023-03-19 00:04:32 +0530

    style: :lipstick: Complete right side

--- src/components/TableModal/TableModal.js
+++ src/components/TableModal/TableModal.js
@@ -88,11 +88,156 @@ const TableModal = (props) => {
                 <p>Side Hustle</p>
               </div>
             </div>
-            <div className="modal-drawer_right">32</div>
-          </div>
 
-          <div className="footer">dsd</div>
+            <div style={{ width: "75%" }}>
+              <div className="modal-drawer_right">
+                <div>
+                  <p>Links</p>
+                </div>
+                <div>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    width="24"
+                    height="24"
+                  >
+                    <path fill="none" d="M0 0h24v24H0z" />
+                    <path d="M13 21v2h-2v-2H3a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h6a3.99 3.99 0 0 1 3 1.354A3.99 3.99 0 0 1 15 3h6a1 1 0 0 1 1 1v16a1 1 0 0 1-1 1h-8zm7-2V5h-5a2 2 0 0 0-2 2v12h7zm-9 0V7a2 2 0 0 0-2-2H4v14h7z" />
+                  </svg>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    width="24"
+                    height="24"
+                  >
+                    <path fill="none" d="M0 0h24v24H0z" />
+                    <path d="M5 2h14a1 1 0 0 1 1 1v19.143a.5.5 0 0 1-.766.424L12 18.03l-7.234 4.536A.5.5 0 0 1 4 22.143V3a1 1 0 0 1 1-1zm13 2H6v15.432l6-3.761 6 3.761V4z" />
+                  </svg>
+                </div>
+              </div>
+
+              {/* Height and Scrollable Auto */}
+              <div style={{ height: "50%", overflowY: "scroll" }}>
+                <div className="modal-drawer_right__element">
+                  {" "}
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    width="24"
+                    height="24"
+                  >
+                    <path fill="none" d="M0 0h24v24H0z" />
+                    <path
+                      d="M1 3h4l7 12 7-12h4L12 22 1 3zm8.667 0L12 7l2.333-4h4.035L12 14 5.632 3h4.035z"
+                      fill="rgba(67,150,202,1)"
+                    />
+                  </svg>
+                  <p>
+                    The golden age of the streaming wars has ended - The Verge
+                  </p>
+                </div>
+
+                <div className="modal-drawer_right__element">
+                  {" "}
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    width="24"
+                    height="24"
+                  >
+                    <path fill="none" d="M0 0h24v24H0z" />
+                    <path
+                      d="M3 3h18a1 1 0 0 1 1 1v16a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1zm10 10v-1h3v-2h-2.586l2.122-2.121-1.415-1.415L12 8.586 9.879 6.464 8.464 7.88 10.586 10H8v2h3v1H8v2h3v2h2v-2h3v-2h-3z"
+                      fill="rgba(0,122,62,1)"
+                    />
+                  </svg>
+                  <p>
+                    Elon Jet, the Twitter account tracking Elon Musk’s flights,
+                    was permanently suspended | TechCrunch
+                  </p>
+                </div>
+                <div className="modal-drawer_right__element">
+                  {" "}
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    width="24"
+                    height="24"
+                  >
+                    <path fill="none" d="M0 0h24v24H0z" />
+                    <path
+                      d="M10.5 17H4v-2h6.5a3.5 3.5 0 1 1-3.278 4.73l1.873-.703A1.5 1.5 0 1 0 10.5 17zM5 11h13.5a3.5 3.5 0 1 1-3.278 4.73l1.873-.703A1.5 1.5 0 1 0 18.5 13H5a3 3 0 0 1 0-6h8.5a1.5 1.5 0 1 0-1.405-2.027l-1.873-.702A3.501 3.501 0 0 1 17 5.5 3.5 3.5 0 0 1 13.5 9H5a1 1 0 1 0 0 2z"
+                      fill="rgba(231,76,60,1)"
+                    />
+                  </svg>
+                  <p>
+                    Tracking layoffs across Asia’s startup ecosystem (Updated) -
+                    Tech in Asia
+                  </p>
+                </div>
+                <div className="modal-drawer_right__element">
+                  {" "}
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    width="24"
+                    height="24"
+                  >
+                    <path fill="none" d="M0 0h24v24H0z" />
+                    <path
+                      d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm-3.5-8v2H11v2h2v-2h1a2.5 2.5 0 1 0 0-5h-4a.5.5 0 1 1 0-1h5.5V8H13V6h-2v2h-1a2.5 2.5 0 0 0 0 5h4a.5.5 0 1 1 0 1H8.5z"
+                      fill="rgba(241,196,14,1)"
+                    />
+                  </svg>
+                  <p>
+                    Musk Shakes Up Twitter’s Legal Team as He Looks to Cut More
+                    Costs - The New York Times
+                  </p>
+                </div>
+                <div className="modal-drawer_right__element">
+                  {" "}
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    width="24"
+                    height="24"
+                  >
+                    <path fill="none" d="M0 0h24v24H0z" />
+                    <path
+                      d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm-3.5-8v2H11v2h2v-2h1a2.5 2.5 0 1 0 0-5h-4a.5.5 0 1 1 0-1h5.5V8H13V6h-2v2h-1a2.5 2.5 0 0 0 0 5h4a.5.5 0 1 1 0 1H8.5z"
+                      fill="rgba(241,196,14,1)"
+                    />
+                  </svg>
+                  <p>
+                    Musk Shakes Up Twitter’s Legal Team as He Looks to Cut More
+                    Costs - The New York Times
+                  </p>
+                </div>
+                <div className="modal-drawer_right__element">
+                  {" "}
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    width="24"
+                    height="24"
+                  >
+                    <path fill="none" d="M0 0h24v24H0z" />
+                    <path
+                      d="M17 3h4a1 1 0 0 1 1 1v16a1 1 0 0 1-1 1h-4V3zM2.859 2.877l12.57-1.795a.5.5 0 0 1 .571.495v20.846a.5.5 0 0 1-.57.495L2.858 21.123a1 1 0 0 1-.859-.99V3.867a1 1 0 0 1 .859-.99zM11 8v4.989L9 11l-1.99 2L7 8H5v8h2l2-2 2 2h2V8h-2z"
+                      fill="rgba(0,0,0,1)"
+                    />
+                  </svg>
+                  <p>
+                    Musk Shakes Up Twitter’s Legal Team as He Looks to Cut More
+                    Costs - The New York Times
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
+
+        <div className="footer">SDdsdsdsdsdE</div>
       </div>
     </div>
   );

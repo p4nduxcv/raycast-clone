802dfe1806862e9f11d4006a5134eaeae605bb59
Pandula <pandula95@gmail.com>
2023-03-18 17:46:33 +0530

    style: :lipstick: Body content has been added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import BodyContent from "./components/BodyContent/BodyContent";
 import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div>
       <Header />
+      <BodyContent />
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -18,6 +18,7 @@ const Header = () => {
             <a href="#">Changelog</a>
             <a href="#">Blog</a>
             <a href="#">Jobs</a>
+            <a href="#">|</a>
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
